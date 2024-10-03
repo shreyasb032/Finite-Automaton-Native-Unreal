75ba3551ba3ee722bc3378f7860eab3556688482
shreyasb032 <shreyas1335@gmail.com>
2024-10-03 16:43:47 -0400

    Updated wait time computation and possible interaction computation

--- Source/FiniteAutomatonModel/Public/Features.h
+++ Source/FiniteAutomatonModel/Public/Features.h
@@ -38,6 +38,9 @@ public:
 	FVector2D agv_speed;
 
 	UPROPERTY(BlueprintReadOnly)
+	FVector2D agv_velocity;
+
+	UPROPERTY(BlueprintReadOnly)
 	FVector2D user_velocity;
 
 	UPROPERTY(BlueprintReadOnly)
@@ -46,7 +49,7 @@ public:
 	bool intent_to_cross;
 	int gazing_station;
 	float gazing_station_cos;
-	//bool possible_interaction;
+	bool possible_interaction;
 	bool facing_sidewalk;
 	bool facing_road;
 	bool on_sidewalk;
@@ -55,15 +58,18 @@ public:
 	FVector2D distance_to_closest_station;
 	bool looking_at_agv;
 
+	bool agv_passed; // whether the agv has already passed the participant
+	bool begin_wait_flag; // a flag to set when the participant begins waiting to cross
+
 	// Station info
-	int start_station_id;
-	int end_station_id;
+	//int start_station_id;
+	//int end_station_id;
 
 	// Start and end station features
-	FVector2D distance_from_start_station;
-	FVector2D distance_from_end_station;
-	bool facing_start_station;
-	bool facing_end_station;
+	//FVector2D distance_from_start_station;
+	//FVector2D distance_from_end_station;
+	//bool facing_start_station;
+	//bool facing_end_station;
 
 	// constants
 	Constants constants;
@@ -75,10 +81,11 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void SetRawFeatures(float user_x, float user_y, float agv_x, float agv_y,
 		float gaze_vector_x, float gaze_vector_y, float gaze_vector_z, float user_yaw);
-	
+	/*
 	// This will be called once per trajectory (or AGV)
 	UFUNCTION(BlueprintCallable)
 	void SetStationInfo(int start_station, int end_station);
+	*/
 
 	UFUNCTION(BlueprintCallable)
 	void GenerateRemainingFeatures(AFeatures* previous);
@@ -93,11 +100,12 @@ private:
 	bool WithinSidewalkBounds(FVector2D* location, float error_range) const;
 	bool WithinRoadBounds(FVector2D* location, float error_range) const;
 	void ClosestStationComputations();
-	void StartAndEndStationsComputations();
+	//void StartAndEndStationsComputations();
 	bool LookingAtAGV() const;
 	bool FacingSidewalk() const;
 	bool FacingRoad() const;
 	void GazingStationComputations();
 	bool IntentToCrossComputations();
-
+	void WaitTimeComputations(AFeatures* previous);
+	void PossibleInteractionComputations();
 };
